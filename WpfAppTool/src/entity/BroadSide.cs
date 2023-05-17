1275e9436b4dd57eca3ea438c6c6d27060608f6c
hao.yuan <lingduliudu@163.com>
2023-05-17 17:19:24 +0800

    12
    12

---
+++ WpfAppTool/src/entity/BroadSide.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTool.src.entity
+{
+    // 侧边工具
+    public partial class BroadSide: ObservableObject
+    {
+        [ObservableProperty]
+        public string name;
+        [ObservableProperty]
+        public string imageUrl;
+        [ObservableProperty]
+        public int sort;
+    }
+}
---
+++ WpfAppTool/src/model/Index.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfAppTool.src.entity;
+
+namespace WpfAppTool.src.model
+{
+    public partial class Index: ObservableObject
+    {
+        [ObservableProperty]
+        public List<BroadSide> broadsideList=new();
+
+        public Index() {
+            initBroadside();
+        }
+        public void initBroadside() {
+            BroadSide bs = new BroadSide();
+            bs.name = "首页";
+            BroadsideList.Add(bs);
+
+        }
+
+        [RelayCommand]
+        private void Test()
+        {
+            MessageBox.Show("展示数据");
+        }
+    }
+}
