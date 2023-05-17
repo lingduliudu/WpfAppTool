621ff11768a6f6954e26b0501025f55f92f1b864
hao.yuan <lingduliudu@163.com>
2023-05-17 17:53:15 +0800

    12
    12

---
+++ WpfAppTool/src/effect/CanvasMove.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace WpfAppTool.src.effect
+{
+    internal class CanvasMove
+    {
+        private bool isDragging = false;
+        private Point offset;
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            var rectangle = e.Source as UIElement;
+            offset = e.GetPosition(rectangle);
+            rectangle.CaptureMouse();
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var rectangle = e.Source as UIElement;
+            rectangle.ReleaseMouseCapture();
+            isDragging = false;
+        }
+
+        private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                var rectangle = e.Source as UIElement;
+                Point position = e.GetPosition(myCanvas);
+                double x = position.X - offset.X;
+                double y = position.Y - offset.Y;
+                Canvas.SetLeft(rectangle, x);
+                Canvas.SetTop(rectangle, y);
+            }
+        }
+    }
+}
